cbf15a3c3e193ec000cbbd6bb919226639ddf65b
fumtas1k <fumta01a7@gmail.com>
2021-12-26 15:11:01 +0900

    add:jquery追加

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 $(function(){
-
+  $(".tab").find("li").on("click", function(){
+    let $this = $(this);
+    let $these = $this.closest(".tab").find("li")
+    let index = $these.index($this)
+    $these.removeClass("select");
+    $this.addClass("select");
+    $(".content").find("li").addClass("hide");
+    $(".content").find("li").eq(index).removeClass("hide");
+  });
 });
